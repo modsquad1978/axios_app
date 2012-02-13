64e50c5b420705c397392dcbe3da6092eee7b214
modsquad1978 <mark.odonnell@azzurgroup.com>
2012-02-13 04:44:31 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AxiosApp::Application.config.secret_token = '57de600117c0b8f95257c1f95b7c6f67dab523f5ec1d16144de475d68f033fc114de824d77f4682ff2e3e6201e9e99829f896acbf9dc9e87ee00cfd08be86403'
